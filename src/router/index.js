b3f3f14dc1492b6f6c3a3607c137ff99d6d8e90e
plusmail <plus4957@gmail.com>
2025-05-29 11:30:24 +0900

    변환

---
+++ src/router/index.js
@@ -0,0 +1,68 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/AppDashboard.vue";
+import Forms from "../views/AppForms.vue";
+import Tables from "../views/AppTables.vue";
+import UIElements from "../views/UIElements.vue";
+import Login from "../views/AppLogin.vue";
+import Modal from "../views/AppModal.vue";
+import Chart from "../views/ChartView.vue";
+import Card from "../views/CardView.vue";
+import Blank from "../views/BlankView.vue";
+import NotFound from "../views/NotFound.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+    meta: { layout: "empty" },
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/forms",
+    name: "Forms",
+    component: Forms,
+  },
+  {
+    path: "/cards",
+    name: "Cards",
+    component: Card,
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    component: Tables,
+  },
+  {
+    path: "/ui-elements",
+    name: "UIElements",
+    component: UIElements,
+  },
+  {
+    path: "/modal",
+    name: "Modal",
+    component: Modal,
+  },
+  {
+    path: "/charts",
+    name: "Chart",
+    component: Chart,
+  },
+  {
+    path: "/blank",
+    name: "Blank",
+    component: Blank,
+  },
+  { path: "/:pathMatch(.*)*", component: NotFound },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router; 
